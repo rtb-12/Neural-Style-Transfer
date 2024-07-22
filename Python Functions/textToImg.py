953e9a17d6c343d1bd2a1e6941af43687afd8ac1
Ronit <147048280+rtb-12@users.noreply.github.com>
2024-07-22 17:36:30 +0530

    Update textToImg.py

--- Python Functions/textToImg.py
+++ Python Functions/textToImg.py
@@ -34,7 +34,7 @@ class CFG:
 
 image_gen_model = StableDiffusionPipeline.from_pretrained(
     CFG.image_gen_model_id, torch_dtype=torch.float16,
-    revision="fp16", use_auth_token='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', guidance_scale=9
+    revision="fp16", use_auth_token='<ENTER_YOUR_HUGGING_FACE_AUTH_TOKEN', guidance_scale=9
 )
 image_gen_model = image_gen_model.to(CFG.device)
 
